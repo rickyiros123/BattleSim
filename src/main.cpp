36a10c86ca5d913651134d69c6bd999630cfd2a0
Ricky Rosales <rickyiros123@gmail.com>
2025-08-10 10:58:26 -0500

    Working on using parsed data

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include <ctime>
 #include <iostream>
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include "json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
 
 struct RollResult
 {
@@ -23,8 +26,8 @@ class Unit
 public:                                                       // Access specifier
   int modelCount, healthPerModel, floatingDamage, save, ward; // Attribute (int variable)
   vector<Weapon> weapons;
-  string unitName;
   vector<string> keywords;
+  string unitName;
 };
 
 RollResult roll_d6(int numberOfDice, int numberOfSides, int desiredRoll, int critValue)
@@ -46,22 +49,24 @@ RollResult roll_d6(int numberOfDice, int numberOfSides, int desiredRoll, int cri
   return rollResult;
 }
 
-// void combatSim(Unit unitA, Unit &unitB)
-// {
-//   int attacks = unitA.models * unitA.attacks;
-//   RollResult attackerHitResult = roll_d6(attacks, 6, unitA.toHit, 6);
-//   RollResult attackerWoundResult = roll_d6(attackerHitResult.successfulRolls, 6, unitA.toWound, 6);
-//   RollResult defenderSavesMade = roll_d6(attackerWoundResult.successfulRolls, 6, unitB.save, 6);
-//   int damage = attackerWoundResult.successfulRolls - defenderSavesMade.successfulRolls;
-//   unitB.totalWounds = unitB.totalWounds - damage;
-//   unitB.models = unitB.totalWounds / unitB.healthPerModel;
-// }
-
 int main()
 {
   std::srand(static_cast<unsigned int>(std::time(nullptr))); // Seed RNG
 
-  RollResult rollResult = roll_d6(20, 6, 5, 6);
-  std::cout << "20d6, 5s to hit: " << rollResult.successfulRolls << " hits and " << rollResult.crits << " crits!" << std::endl;
+  std::ifstream factionData("data/factions/seraphon.json");
+  if (!factionData)
+  {
+    std::cerr << "Could not open factions.json\n";
+    return 1;
+  }
+
+  json jsonData;
+
+  factionData >> jsonData;
+
+  cout << "Unit name: " << jsonData["units"][0]["unitName"].get<std::string>() << "\n";
+
+  // RollResult rollResult = roll_d6(20, 6, 5, 6);
+  // std::cout << "20d6, 5s to hit: " << rollResult.successfulRolls << " hits and " << rollResult.crits << " crits!" << std::endl;
   return 0;
 }
